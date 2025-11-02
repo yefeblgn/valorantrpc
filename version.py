01143f3750c1a99206e3806d7a7ae183ded2e14c
Efe <ucarkacar231415@gmail.com>
2025-11-02 10:20:57 +0300

    1.0.1 Update

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 Sürüm yönetimi
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 GITHUB_REPO = "yefeblgn/valorantrpc"
 GITHUB_RELEASES_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 GITHUB_REPO_URL = f"https://github.com/{GITHUB_REPO}/releases/latest"
